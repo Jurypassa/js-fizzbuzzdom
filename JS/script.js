50081f6f507c9f4f5d85b553f8b382aae60f19c3
Jury <jurypassa@gmail.com>
2021-11-09 18:41:46 +0100

    fine esercizio

--- JS/script.js
+++ JS/script.js
@@ -2,11 +2,14 @@ const elenco = document.querySelector(".lista");
 
 
 for (let elemento = 1; elemento <= 100; elemento++){
-    if (elemento % 3 == 0){
-        const lista = `<li class="box fizz">fizz</li>`;
+    if (elemento % 15 == 0){
+        const lista = `<li class="box fizzbuzz">fizzbuzz</li>`;
         elenco.innerHTML += lista;
     }else if (elemento % 5 == 0){
         const lista = `<li class="box buzz">buzz</li>`;
+        elenco.innerHTML += lista;
+    }else if (elemento % 3 == 0){
+        const lista = `<li class="box fizz">fizz</li>`;
         elenco.innerHTML += lista;
     }else {
         const lista = `<li class="box numero">${elemento}</li>`;
